ab3a34bbf16e0255f4cfb1f75d5e0cf5ef06a868
zerofive12 <navada3390@gmail.com>
2025-06-29 23:55:05 +0700

    Add files via upload

---
+++ you/service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "calendar-offline-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "icon.png",
+  "/"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) =>
+      response || fetch(event.request)
+    )
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+});
